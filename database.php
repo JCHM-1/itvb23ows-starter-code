b8baf241fef33c701a5eb034d4887c82dc6da29e
JCHM-1 <j.c.huizinga.mora@st.hanze.nl>
2024-01-12 20:16:21 +0100

    Implemented SonarQube changes

--- database.php
+++ database.php
@@ -1,11 +1,21 @@
 <?php
+//load .env file
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+use Dotenv\Dotenv;
 
-function get_state()
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$username = $_ENV['MYSQL_ROOT_USER'];
+$password = $_ENV['MYSQL_ROOT_PASSWORD'];
+$database = $_ENV['MYSQL_DATABASE'];
+
+function getState()
 {
     return serialize([$_SESSION['hand'], $_SESSION['board'], $_SESSION['player']]);
 }
 
-function set_state($state)
+function setState($state)
 {
     list($a, $b, $c) = unserialize($state);
     $_SESSION['hand'] = $a;
@@ -13,4 +23,4 @@ function set_state($state)
     $_SESSION['player'] = $c;
 }
 
-return new mysqli('db', 'root', 'Test2001', 'hive');
+return new mysqli('db', $username, $password, $database);
--- util.php
+++ util.php
@@ -5,13 +5,18 @@ $GLOBALS['OFFSETS'] = [[0, 1], [0, -1], [1, 0], [-1, 0], [-1, 1], [1, -1]];
 function isNeighbour($a, $b) {
     $a = explode(',', $a);
     $b = explode(',', $b);
-    if ($a[0] == $b[0] && abs($a[1] - $b[1]) == 1) return true;
-    if ($a[1] == $b[1] && abs($a[0] - $b[0]) == 1) return true;
-    if ($a[0] + $a[1] == $b[0] + $b[1]) return true;
-    return false;
+    $isNeighbour = false;
+
+    if (($a[0] == $b[0] && abs($a[1] - $b[1]) == 1) ||
+        ($a[1] == $b[1] && abs($a[0] - $b[0]) == 1) ||
+        ($a[0] + $a[1] == $b[0] + $b[1])) {
+        $isNeighbour = true;
+    }
+
+    return $isNeighbour;
 }
 
-function hasNeighBour($a, $board) {
+function hasNeighbour($a, $board) {
     foreach (array_keys($board) as $b) {
         if (isNeighbour($a, $b)) return true;
     }
@@ -31,17 +36,24 @@ function len($tile) {
 }
 
 function slide($board, $from, $to) {
-    if (!hasNeighBour($to, $board)) return false;
-    if (!isNeighbour($from, $to)) return false;
-    $b = explode(',', $to);
-    $common = [];
-    foreach ($GLOBALS['OFFSETS'] as $pq) {
-        $p = $b[0] + $pq[0];
-        $q = $b[1] + $pq[1];
-        if (isNeighbour($from, $p.",".$q)) $common[] = $p.",".$q;
+    $canSlide = false;
+
+    if (hasNeighBour($to, $board) && isNeighbour($from, $to)) {
+        $b = explode(',', $to);
+        $common = [];
+        foreach ($GLOBALS['OFFSETS'] as $pq) {
+            $p = $b[0] + $pq[0];
+            $q = $b[1] + $pq[1];
+            if (isNeighbour($from, $p.",".$q)) {
+                $common[] = $p.",".$q;
+            }
+        }
+
+        if ($board[$common[0]] || $board[$common[1]] || $board[$from] || $board[$to]) {
+            $canSlide = min(len($board[$common[0]]), len($board[$common[1]])) <= max(len($board[$from]), len($board[$to]));
+        }
     }
-    if (!$board[$common[0]] && !$board[$common[1]] && !$board[$from] && !$board[$to]) return false;
-    return min(len($board[$common[0]]), len($board[$common[1]])) <= max(len($board[$from]), len($board[$to]));
+
+    return $canSlide;
 }
 
-?>
